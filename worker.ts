3cb303a60b2d8664727ad3d8bc161b36087d798a
Aidan <a.lavis@kickstarter.com>
2025-12-23 14:46:39 -0500

    use custom worker script

---
+++ worker.ts
@@ -0,0 +1,19 @@
+import astroWorker from "./dist/_worker.js/index.js";
+
+export default {
+  async fetch(request: Request, env: Env, ctx: ExecutionContext) {
+    const url = new URL(request.url);
+
+    if (url.pathname.startsWith("/projects/")) {
+      const response = (await astroWorker.fetch(request, env, ctx)) as Response;
+
+      response.headers.set(
+        "Cache-Control",
+        "public, max-age=3600, s-maxage=3600"
+      );
+    }
+
+    // Pass through all other requests to Astro worker
+    return astroWorker.fetch(request, env, ctx);
+  },
+};
